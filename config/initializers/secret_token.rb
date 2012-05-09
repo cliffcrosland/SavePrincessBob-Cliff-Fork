80ed0efecb2218b761b45e2daba47416212ae62e
Jon Awesome Pulsipher <mistermuerte@gmail.com>
2012-05-09 16:52:46 -0700

    Initialize files (fool)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SavePrincessBob::Application.config.secret_token = '720b3d464e2e3b2124cf20fabac5eae465602de73e0c1d91561a470044b8b871e57861a266e3480fdd090d9e56bba98099d3c97df50b390e4a8caf9e3b261108'
