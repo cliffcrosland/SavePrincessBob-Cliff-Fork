46b7a58893d7ae01651c6e4e312bc2f2808de62b
Clifton Crosland <cliffcrosland@gmail.com>
2012-05-09 19:44:02 -0700

    Add Home controller.  Add Game and Character models (a Game has many Character subfields).

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def flash_game
+  end
+
+  def the_end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 SavePrincessBob::Application.routes.draw do
+  get "home/index"
+  get "home/flash_game"
+  get "home/the_end"
+
+  resources :games
+
+  resources :characters
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +56,7 @@ SavePrincessBob::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
